097c9f8bd48c6fe04f2f8bb8b66eb76735bc60bb
Hunter Haufler <haufhun@gmail.com>
2025-06-20 18:45:23 -0500

    Move Other Screens Out of Tabs

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -55,15 +55,6 @@ export default function TabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
-        name="example"
-        options={{
-          title: "example",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          ),
-        }}
-      />
     </Tabs>
   );
 }
