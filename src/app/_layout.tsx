470f9af0e1a0fed2419424fe8e82533e84d5d29d
Hunter Haufler <haufhun@gmail.com>
2025-07-18 19:17:35 -0500

    Refactor RootLayout to use a fixed light theme and remove unused dark theme logic

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,8 @@
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { useColorScheme } from "@/hooks/useColorScheme";
+// import { useColorScheme } from "@/hooks/useColorScheme";
 import AuthProvider from "@/providers/auth-provider";
 import QueryProvider from "@/providers/query-provider";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
@@ -17,7 +13,8 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
+  const colorScheme = "light"; // Default to light theme for now
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -32,7 +29,8 @@ export default function RootLayout() {
         <AuthProvider>
           <QueryProvider>
             <ThemeProvider
-              value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+              // value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+              value={DefaultTheme}
             >
               <GestureHandlerRootView>
                 <Stack
